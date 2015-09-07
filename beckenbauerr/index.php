8271f430df3405fd122c36f58078a55e86bc1aae
edpo <edpo_75@hotmail.com>
2015-09-07 13:49:11 -0600

    Actualizacion 07/09/2015 1:30 p.m

--- beckenbauerr/index.php
+++ beckenbauerr/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Beckenbauer.</title>
+  <title>Beckenbauer..</title>
   <?php include 'inc/header.php';?>
   <style>
   .carousel-inner > .item > img,
